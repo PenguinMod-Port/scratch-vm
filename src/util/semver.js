46b5f98bab3c7a63b180fe2adf300d5d76780b29
jwklong <72522395+jwklong@users.noreply.github.com>
2025-12-30 21:46:27 +0000

    add support for stuff like 0.1.x to semver

--- src/util/semver.js
+++ src/util/semver.js
@@ -1,4 +1,4 @@
-const semverRegex = /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/;
+const semverRegex = /^(0|x|[1-9]\d*)\.(0|x|[1-9]\d*)\.(0|x|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/;
 
 class SemVer {
     constructor(str) {
@@ -6,9 +6,9 @@ class SemVer {
         if (!match) {
             throw new Error(`Invalid semver: ${str}`);
         }
-        this.major = parseInt(match[1], 10);
-        this.minor = parseInt(match[2], 10);
-        this.patch = parseInt(match[3], 10);
+        this.major = match[1] === "x" ? "x" : parseInt(match[1], 10);
+        this.minor = match[2] === "x" ? "x" : parseInt(match[2], 10);
+        this.patch = match[3] === "x" ? "x" : parseInt(match[3], 10);
         this.prerelease = match[4];
         this.build = match[5];
     }
@@ -23,13 +23,19 @@ class SemVer {
     }
 
     compare(other) {
-        other = SemVer.convert();
-        if (this.major > other.major) return 1;
-        if (this.major < other.major) return -1;
-        if (this.minor > other.minor) return 1;
-        if (this.minor < other.minor) return -1;
-        if (this.patch > other.patch) return 1;
-        if (this.patch < other.patch) return -1;
+        other = SemVer.convert(other);
+        if (this.major !== "x" && other.major !== "x") {
+            if (this.major > other.major) return 1;
+            if (this.major < other.major) return -1;
+        }
+        if (this.minor !== "x" && other.minor !== "x") {
+            if (this.minor > other.minor) return 1;
+            if (this.minor < other.minor) return -1;
+        }
+        if (this.patch !== "x" && other.patch !== "x") {
+            if (this.patch > other.patch) return 1;
+            if (this.patch < other.patch) return -1;
+        }
         if (this.prerelease > other.prerelease) return 1;
         if (this.prerelease < other.prerelease) return -1;
         return 0;
@@ -60,6 +66,8 @@ class SemVer {
     }
 
     withinRange(min, max) {
+        min = SemVer.convert(min);
+        max = SemVer.convert(max);
         if (min.greaterThan(max)) {
             const temp = min;
             min = max;
