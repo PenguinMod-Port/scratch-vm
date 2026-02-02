a9a6d457a02ac83537c7f8dc8111de4b8c86fb4a
jwklong <72522395+jwklong@users.noreply.github.com>
2026-02-02 16:27:48 +0000

    add isPenguinMod constant

--- src/extension-support/extension-worker.js
+++ src/extension-support/extension-worker.js
@@ -96,6 +96,7 @@ Object.assign(global.Scratch, ScratchCommon, {
  */
 const extensionWorker = new ExtensionWorker();
 global.Scratch.extensions = {
+    isPenguinMod: true,
     register: extensionWorker.register.bind(extensionWorker)
 };
 
