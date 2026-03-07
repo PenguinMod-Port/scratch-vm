4b001cc9626c619cb2d71f78cfea98816ecdf9bf
jwklong <jwklong@gmx.com>
2026-03-07 16:08:31 +0000

    dont Make all blocks external pls

--- src/serialization/sb3.js
+++ src/serialization/sb3.js
@@ -1294,6 +1294,7 @@ const parseScratchObject = function (object, runtime, pmVersion, extensions, zip
                 Variable.SCALAR_TYPE, // type of the variable
                 isCloud
             );
+            console.log(variable);
             if (isCloud) runtime.addCloudVariable();
             newVariable.value = variable[1];
             target.variables[newVariable.id] = newVariable;
