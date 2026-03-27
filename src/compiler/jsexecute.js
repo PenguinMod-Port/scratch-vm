cb755e10cbbfe7810c1b58f5dbb58b515978ee17
jwklong <jwklong@gmx.com>
2026-03-27 17:35:31 +0000

    make it throw error

--- src/compiler/jsexecute.js
+++ src/compiler/jsexecute.js
@@ -122,9 +122,7 @@ const waitPromise = function*(promise) {
             thread.status = 0; // STATUS_RUNNING
         }, error => {
             thread.status = 0; // STATUS_RUNNING
-            globalState.log.warn('Promise rejected in compiled script:', error);
-            returnValue = '' + error;
-            thread.status = 0; // STATUS_RUNNING
+            throw error;
         });
 
     yield;
