cc74dbaf30536e255b04e570a11a4846a73f5f11
jwklong <jwklong@gmx.com>
2026-05-14 17:13:58 +0100

    bitwise & coalescing opexpansion

--- src/extensions/penguinmod/pmOperatorsExpansion/index.js
+++ src/extensions/penguinmod/pmOperatorsExpansion/index.js
@@ -2,22 +2,23 @@ const BlockType = require('../../../extension-support/block-type');
 const ArgumentType = require('../../../extension-support/argument-type');
 const Cast = require('../../../util/cast');
 
+const template = {
+    extensions: ["colours_operators"]
+};
+
 function generateJoin(inputs) {
     return {
         opcode: `join${inputs}`,
         func: 'joinMultiple',
-        text: `join${Array(inputs).fill().map((_, i) => ` [STRING${i+1}]`).join("")}`,
+        text: `join${Array(inputs).fill().map((_, i) => ` [STRING${i + 1}]`).join("")}`,
         blockType: BlockType.REPORTER,
-        arguments: Object.fromEntries(Array(inputs).fill().map((_, i) => [`STRING${i+1}`, {
+        arguments: Object.fromEntries(Array(inputs).fill().map((_, i) => [`STRING${i + 1}`, {
             type: ArgumentType.STRING
         }])),
-        hideFromPalette: true
+        hideFromPalette: true,
+        ...template
     };
 }
-
-const template = {
-    extensions: ["colours_operators"]
-};
 
 class Extension {
     getInfo() {
@@ -47,7 +48,151 @@ class Extension {
                         }
                     },
                     ...template
-                }
+                },
+                "---",
+                {
+                    opcode: "orIfFalsey",
+                    text: "[ONE] || [TWO]",
+                    blockType: BlockType.REPORTER,
+                    allowDropAnywhere: true,
+                    arguments: {
+                        ONE: {
+                            type: ArgumentType.STRING,
+                            defaultValue: "a"
+                        },
+                        TWO: {
+                            type: ArgumentType.STRING,
+                            defaultValue: "b"
+                        }
+                    },
+                    ...template
+                },
+                {
+                    opcode: "nullishCoalescing",
+                    text: "[ONE] ?? [TWO]",
+                    blockType: BlockType.REPORTER,
+                    allowDropAnywhere: true,
+                    arguments: {
+                        ONE: {
+                            type: ArgumentType.STRING,
+                            defaultValue: "a"
+                        },
+                        TWO: {
+                            type: ArgumentType.STRING,
+                            defaultValue: "b"
+                        }
+                    },
+                    ...template
+                },
+                {
+                    opcode: "ifIsTruthy",
+                    text: "if [ONE] is true then [TWO]",
+                    blockType: BlockType.REPORTER,
+                    allowDropAnywhere: true,
+                    arguments: {
+                        ONE: {
+                            type: ArgumentType.BOOLEAN
+                        },
+                        TWO: {
+                            type: ArgumentType.STRING,
+                        }
+                    },
+                    hideFromPalette: true,
+                    ...template
+                },
+                "---",
+                {
+                    opcode: "binnaryAnd",
+                    text: "[num1] and [num2]",
+                    blockType: BlockType.REPORTER,
+                    arguments: {
+                        num1: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 32
+                        },
+                        num2: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 5
+                        }
+                    },
+                    ...template
+                },
+                {
+                    opcode: "binnaryOr",
+                    text: "[num1] or [num2]",
+                    blockType: BlockType.REPORTER,
+                    arguments: {
+                        num1: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 7
+                        },
+                        num2: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 8
+                        }
+                    },
+                    ...template
+                },
+                {
+                    opcode: "binnaryXor",
+                    text: "[num1] xor [num2]",
+                    blockType: BlockType.REPORTER,
+                    arguments: {
+                        num1: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 7
+                        },
+                        num2: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 2
+                        }
+                    },
+                    ...template
+                },
+                {
+                    opcode: "shiftLeft",
+                    text: "[num1] << [num2]",
+                    blockType: BlockType.REPORTER,
+                    arguments: {
+                        num1: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 1
+                        },
+                        num2: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 5
+                        }
+                    },
+                    ...template
+                },
+                {
+                    opcode: "shiftRight",
+                    text: "[num1] >> [num2]",
+                    blockType: BlockType.REPORTER,
+                    arguments: {
+                        num1: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 32
+                        },
+                        num2: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 5
+                        }
+                    },
+                    ...template
+                },
+                {
+                    opcode: "binnaryNot",
+                    text: "~ [num1]",
+                    blockType: BlockType.REPORTER,
+                    arguments: {
+                        num1: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 2
+                        }
+                    },
+                    ...template
+                },
             ]
         };
     }
@@ -68,6 +213,59 @@ class Extension {
             else if (INPUT.customId) return INPUT.customId;
         }
         return 'unknown';
+    }
+
+    orIfFalsey({ONE, TWO}) {
+        return ONE || TWO;
+    }
+
+    nullishCoalescing({ONE, TWO}) {
+        return ONE ?? TWO;
+    }
+
+    ifIsTruthy({ONE, TWO}) {
+        return Cast.toBoolean(ONE) && TWO;
+    }
+
+    binnaryAnd({num1, num2}) {
+        num1 = Cast.toNumber(num1);
+        num2 = Cast.toNumber(num2);
+
+        return num1 & num2;
+    }
+
+    binnaryOr({num1, num2}) {
+        num1 = Cast.toNumber(num1);
+        num2 = Cast.toNumber(num2);
+
+        return num1 | num2;
+    }
+
+    binnaryXor({num1, num2}) {
+        num1 = Cast.toNumber(num1);
+        num2 = Cast.toNumber(num2);
+
+        return num1 ^ num2;
+    }
+
+    shiftLeft({num1, num2}) {
+        num1 = Cast.toNumber(num1);
+        num2 = Cast.toNumber(num2);
+
+        return num1 << num2;
+    }
+
+    shiftRight({num1, num2}) {
+        num1 = Cast.toNumber(num1);
+        num2 = Cast.toNumber(num2);
+
+        return num1 >> num2;
+    }
+
+    binnaryNot({num1}) {
+        num1 = Cast.toNumber(num1);
+
+        return ~num1;
     }
 }
 
