54bad4cec68c9737c75fa83b33cf855de9942b0f
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2026-09-02 14:19:21 -0700

    Fix parsing error

--- src/engine/comment.js
+++ src/engine/comment.js
@@ -31,9 +31,9 @@ class Comment {
     }
 
     toXML () {
-        return `<comment id="${this.id}" x="${this.x}" y="${this.y}"`
-            + `w="${this.width}" h="${this.height}"`
-            + `pinned="${this.blockId !== null}" minimized="${this.minimized}"`
+        return `<comment id="${this.id}" x="${this.x}" y="${this.y}" `
+            + `w="${this.width}" h="${this.height}" `
+            + `pinned="${this.blockId !== null}" minimized="${this.minimized}" `
             + `data="${xmlEscape(JSON.stringify(this.data))}">${xmlEscape(this.text)}`
             + `</comment>`;
     }
