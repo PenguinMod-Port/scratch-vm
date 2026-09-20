203b665bbcb9292c5ef6831fd2d5ebe82da94043
jwklong <jwklong@gmx.com>
2026-09-20 15:20:07 +0100

    wip
    Signed-off-by: jwklong <jwklong@gmx.com>

--- src/extensions/penguinmod/jwDebugger/index.js
+++ src/extensions/penguinmod/jwDebugger/index.js
@@ -3,10 +3,53 @@ const BlockShape = require('../../../extension-support/block-shape')
 const ArgumentType = require('../../../extension-support/argument-type')
 const Cast = require('../../../util/cast')
 
+const createElement = (type, properties = {}, parent) => {
+    const el = document.createElement(type);
+    Object.entries(properties).forEach(([k, v]) => el[k] = v);
+    if (parent) parent.appendChild(el);
+    return el;
+}
+
+const style = `
+.jwDebugger-root {
+    display: flex;
+    flex-direction: column;
+    padding: 1em;
+    height: 100%;
+}
+
+.jwDebugger-list {
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    border: 1px solid var(--ui-black-transparent);
+    border-radius: 8px;
+    overflow-y: scroll;
+}
+
+.jwDebugger-list > * {
+    padding: 0.5em;
+    background-color: var(--ui-primary);
+}
+.jwDebugger-list > *:nth-child(even) {
+    background-color: var(--ui-secondary);
+}
+
+.jwDebugger-timestamp {
+    font-size: 0.75em;
+    opacity: 0.5;
+    padding-right: 0.5em;
+}
+`
+
 class Extension {
     constructor() {
         this.tab = vm.runtime.tabManager.register("jwDebugger", "Debugger");
-        this.tab.setDOM(document.createElement('div'));
+        this.rootElement = createElement('div', {className: "jwDebugger-root"});
+        this.tab.setDOM(this.rootElement);
+        createElement('style', {innerHTML: style}, document.head);
+
+        this.listElement = createElement('div', {className: "jwDebugger-list"}, this.rootElement);
     }
 
     getInfo() {
@@ -50,13 +93,23 @@ class Extension {
     }
 
     log({LOG, DATA}) {
-        const span = document.createElement('span');
-        span.innerText = String(DATA);
-        this.tab.element.append(span);
+        const now = new Date(Date.now())
+        const timestamp = `${String(now.getHours()).padStart(2, "0")}:${String(now.getMinutes()).padStart(2, "0")}:${String(now.getSeconds()).padStart(2, "0")}.${String(now.getMilliseconds()).padStart(3, "0")}`
+
+        let log = createElement('div', {
+            className: `jwDebugger-${LOG}`
+        }, this.listElement);
+        createElement('span', {
+            className: 'jwDebugger-timestamp',
+            innerText: timestamp
+        }, log);
+        createElement('span', {
+            innerText: String(DATA)
+        }, log);
     }
 
     clear() {
-        this.tab.element.innerHTML = '';
+        this.listElement.innerHTML = '';
     }
 }
 
